b2e31f591e8ee5137f96cc740eddd3f573f3dc24
Simon Sirak <simon.sirak@gmail.com>
2019-01-06 12:17:36 +0100

    Create boba.c

---
+++ assignments/fork/boba.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Dont't get in my way!");
+    printf("Dont't get in my way!");
+    printf("Dont't get in my way!");
+    printf("Dont't get in my way!");
+    printf("Dont't get in my way!");
+}
